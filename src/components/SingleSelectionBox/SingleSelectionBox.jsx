4bf954052cacda20d29b9252c2cad8fffcae30af
SyedaMahamFahim <fahim4303107@cloud.neduet.edu.pk>
2022-04-06 16:50:40 +0500

    sort-selection-completed

--- src/components/SingleSelectionBox/SingleSelectionBox.jsx
+++ src/components/SingleSelectionBox/SingleSelectionBox.jsx
@@ -1,11 +1,13 @@
-// import React, { useState, useEffect } from "react";
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import React from "react";
 
 import "./singleSelectionBox.css";
 // import Select from "react-select";
-// import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 const SingleSelectionBox = ({ data }) => {
- 
+  const [optionVal, setOptionVal] = useState("Relevance");
+  const [toggle, setToggle] = useState(false);
+
   return (
     <>
       <div className="single-selection__sort_box">
@@ -15,22 +17,48 @@ const SingleSelectionBox = ({ data }) => {
               color: "#808184",
             }}
           >
-            Sort By 
+            Sort By <>&nbsp;</> <span>{optionVal}</span>
+          {toggle ? (
+            <span onClick={() => setToggle(!toggle)}>
+              <IoIosArrowUp />
+            </span>
+          ) : (
+            <span onClick={() => setToggle(!toggle)}>
+              <IoIosArrowDown />
+            </span>
+          )}
           </p>
         </div>
-        <select >
-        
-            {data.map((val, index) => (
-              <option value={val.value} key={index} >
-                {val.label}
-           
-              </option>
-            ))}
-         
-          </select>
-        </div>
+       
+      </div>
 
+      {toggle && (
+        <div className="single-selection__sort_box_select">
+          {data.map((val, index) => (
+            <p key={index} onClick={() => setOptionVal(val.label)}>
+              {
+                optionVal === val.label && <span>
+                <>&#10004; &nbsp;</>
+              </span>
+              }
+              
+
+              {val.label}
+            </p>
+          ))}
+        </div>
+      )}
+
+      {/* <select >
         
+        {data.map((val, index) => (
+          <option value={val.value} key={index} >
+            {val.label}
+       
+          </option>
+        ))}
+     
+      </select> */}
     </>
   );
 };
